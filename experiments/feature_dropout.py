e79d4d211faa9ab0ae189fcc15413deb93150fb4
judewells <judewells@hotmail.com>
2022-03-28 11:08:16 +0100

    create feature dropout script

---
+++ experiments/feature_dropout.py
@@ -0,0 +1,22 @@
+from test_alphafold_classifer_w_preprocessed import combined_features, fit_and_evaluate, add_geometricus_features, drop_missing_alphafold_rows, get_scores
+import pandas as pd
+
+
+if __name__ == '__main__':
+    target = 'PPI_interface_true'
+    train = pd.read_csv('annotated_processed_training_with_alphafold.csv')
+    test = pd.read_csv('annotated_processed_validation_with_alphafold.csv')
+    geometricus_train_path = '../datasets/PPI/geometricus_PPI_training_dataset.csv'
+    geometricus_test_path = '../datasets/PPI/geometricus_PPI_validation_dataset.csv'
+    # train, test = add_geometricus_features(train, test, geometricus_train_path, geometricus_test_path)
+    train = drop_missing_alphafold_rows(train)
+    test = drop_missing_alphafold_rows(test)
+    results = pd.DataFrame(index=combined_features, columns=['train_pr_auc','accuracy','ROC AUC', 'PR AUC',])
+    for drop_feature in combined_features:
+        if 'residue_aa' in drop_feature:
+            continue
+        eval_dict = fit_and_evaluate(train, test, target=target, use_alphafold=True, use_ff=True,
+                                       use_geometricus=False, drop_features=[drop_feature])
+        del eval_dict['probas']
+        results.loc[drop_feature, eval_dict.keys()] = eval_dict.values()
+
--- experiments/test_alphafold_classifer_w_preprocessed.py
+++ experiments/test_alphafold_classifer_w_preprocessed.py
@@ -8,12 +8,23 @@ from sklearn.metrics import accuracy_score, roc_auc_score, precision_recall_curv
 This script assumes that there are DFs stored on disk that already have alphafold features
 where alphafold features are stored as columns labeled 0-384
 """
+
 features_ff = ['scons','avg_scons','sc5_gs','sc5_scons', 'conserved_hotspot_struc_neighbourhood', 'conserved_surface_hotspot_struc_neighbourhood', 'highly_conserved_struc_neighbourhood', 'highly_conserved_surface_struc_neighbourhood', 'pocket_conserved_struc_neighbourhood', 'pocket_surface_conserved_struc_neighbourhood',
                'avg_charged', 'avg_cx', 'avg_dpx', 'avg_electric_effect', 'avg_flexibility', 'avg_hydropathicity', 'avg_hydrophobicity', 'avg_polarity', 'avg_surface_residues', 'avg_surrounding_hydrophobicity', 'dist_to_hotspot', 'dist_to_surface', 'hotspot_struc_neighbourhood', 'pocket_struc_neighbourhood', 'surface_residues_struc_neighbourhood', 'min_dist_to_cleft123', 'min_dist_to_cleft_1', 'min_dist_to_cleft_2', 'min_dist_to_cleft_3', 'surface_residue_rsa', 'cleft_residue', 'hydrophobic_aa', 'polar_aa',
                'alpha','betweenness','bulkiness','charge','cleft_depth','cleft_num','closeness','degree','foldx_alascan','free_energy_solution','hydration_potential','hydropathicity','hydropathy_index','hydrophobicity','hydrophobicity_psaia','kappa','localised_electrical_effect','max_cx','max_dpx','min_cx','min_dpx','nhBonds_ptr','oBonds_ptr','phi','polarity','psi','resTco','res_bfactor_n','rsa_allatoms','rsa_mainchain','rsa_nonpolar','rsa_polar','rsa_totside','van_der_waals_vol_normalised',
                'dssp_type_B','dssp_type_H','dssp_type_NO_PRED','dssp_type_T',
                'A_pssm_ff','A_wop_ff','C_pssm_ff','C_wop_ff','D_pssm_ff','D_wop_ff','E_pssm_ff','E_wop_ff','F_pssm_ff','F_wop_ff','G_pssm_ff','G_wop_ff','H_pssm_ff','H_wop_ff','I_pssm_ff','I_wop_ff','K_pssm_ff','K_wop_ff','L_pssm_ff','L_wop_ff','M_pssm_ff','M_wop_ff','N_pssm_ff','N_wop_ff','P_pssm_ff','P_wop_ff','Q_pssm_ff','Q_wop_ff','R_pssm_ff','R_wop_ff','S_pssm_ff','S_wop_ff','T_pssm_ff','T_wop_ff','V_pssm_ff','V_wop_ff','W_pssm_ff','W_wop_ff','Y_pssm_ff','Y_wop_ff','gapless_match_to_pseudocounts_ff','info_per_pos_ff',
               ]
+
+features_generic = [
+               'bulkiness','charge','cleft_depth','cleft_num','hydration_potential','hydropathicity','hydropathy_index','hydrophobicity','localised_electrical_effect','max_cx','max_dpx','min_cx','min_dpx','mutability','polarity','psi','resTco','res_bfactor_n','rsa_allatoms','van_der_waals_vol_normalised','dssp_type_B','dssp_type_H','dssp_type_NO_PRED','dssp_type_T',
+               'residue_aa_A','residue_aa_C','residue_aa_D','residue_aa_E','residue_aa_F','residue_aa_G','residue_aa_H','residue_aa_I','residue_aa_K','residue_aa_L','residue_aa_M','residue_aa_N','residue_aa_P','residue_aa_Q','residue_aa_R','residue_aa_S','residue_aa_T','residue_aa_V','residue_aa_W','residue_aa_Y',
+               'A_pssm_psiblast','A_wop_psiblast','C_pssm_psiblast','C_wop_psiblast','D_pssm_psiblast','D_wop_psiblast','E_pssm_psiblast','E_wop_psiblast','F_pssm_psiblast','F_wop_psiblast','G_pssm_psiblast','G_wop_psiblast','H_pssm_psiblast','H_wop_psiblast','I_pssm_psiblast','I_wop_psiblast','S_pssm_psiblast','K_pssm_psiblast','K_wop_psiblast','L_pssm_psiblast','L_wop_psiblast','M_pssm_psiblast','M_wop_psiblast','R_wop_psiblast','N_pssm_psiblast','N_wop_psiblast','P_pssm_psiblast','P_wop_psiblast','Q_pssm_psiblast','Q_wop_psiblast','R_pssm_psiblast','S_wop_psiblast','T_pssm_psiblast','T_wop_psiblast','V_pssm_psiblast','V_wop_psiblast','Y_wop_psiblast','Y_pssm_psiblast','W_wop_psiblast','W_pssm_psiblast','info_per_pos_psiblast','gapless_match_to_pseudocounts_psiblast','entwop_score_psiblast',
+              ]
+
+features_generic = [f for f in features_generic if f not in features_ff]
+
+combined_features = features_ff + features_generic
 
 def domain_train_test_split(df, split_by_domain=True, train_prop = 0.7):
 
@@ -30,7 +41,7 @@ def domain_train_test_split(df, split_by_domain=True, train_prop = 0.7):
         test = df.iloc[last_train_idx:]
     return train, test
 
-def fit_and_evaluate(train, test, target='res_label', use_alphafold=True, validation=None, use_ff=True, use_geometricus=False):
+def fit_and_evaluate(train, test, target='res_label', use_alphafold=True, validation=None, use_ff=True, use_geometricus=False, use_generic=True, drop_features=None):
     """
     Note that this version of the function assumes alphafold features have been
     pre-computed and exist in the dataframe as columns labeled 0-383
@@ -38,11 +49,14 @@ def fit_and_evaluate(train, test, target='res_label', use_alphafold=True, valida
     features = []
     if use_ff:
         features += features_ff
+    if use_generic:
+        features += features_generic
     if use_alphafold:
         features += [str(i) for i in range(384)]
     if use_geometricus:
         features += ['k1', 'k2', 'k3', 'k4', 'r1', 'r2', 'r3', 'r4']
-
+    if drop_features is not None:
+        features = [f for f in features if f not in drop_features]
     train_x = train[features]
     test_x = test[features]
     if validation is not None:
@@ -54,8 +68,6 @@ def fit_and_evaluate(train, test, target='res_label', use_alphafold=True, valida
     train_y = train[target]
     test_y = test[target]
     model = xgb.XGBClassifier(
-        tree_method='gpu_hist',
-        gpu_id=0,
         n_estimators=2000,
         learning_rate=0.01,
         subsample=0.7,
@@ -83,21 +95,17 @@ def fit_and_evaluate(train, test, target='res_label', use_alphafold=True, valida
         model.fit(train_x, train_y, eval_metric=loss)
     train_time = time.time() - start
     print(f'Training time {train_time}')
-    preds = model.predict(test_x)
     train_probas = model.predict_proba(train_x)[:,1]
-    train_precision, train_recall, train_thresholds = precision_recall_curve(train_y, train_probas)
-    train_pr_auc = auc(train_recall, train_precision)
     probas = model.predict_proba(test_x)[:,1]
-    print(preds[:10], probas[:10])
-    precision, recall, thresholds = precision_recall_curve(test_y, probas)
-    pr_auc = auc(recall, precision)
+    preds = [1 if p >= 0.5 else 0 for p in probas]
+    scores = get_scores(test_y, probas, preds, train_y, train_probas)
+
     print(f'DF shape: {train_x.shape}')
     print(f'Test shape {test_x.shape}')
-    print(f'TRAINING PR AUC: {train_pr_auc}')
-    print(f'accuracy: {accuracy_score(test_y, preds)}')
-    print(f'ROC AUC: {roc_auc_score(test_y, probas)} ')
-    print(f'PR AUC: {pr_auc} ')
-    return probas
+    for k, v in scores.items():
+        print(f'{k}: {v}')
+    scores['probas'] = probas
+    return scores
 
 def drop_missing_alphafold_rows(df):
     if 'alpha_rep' in df.columns:
@@ -121,27 +129,40 @@ def add_geometricus_features(train, test, geo_train_path, geo_test_path):
     del test_geometricus
     return train, test
 
+def get_scores(test_y, probas, preds, train_y, train_probas):
+    precision, recall, thresholds = precision_recall_curve(test_y, probas)
+    train_precision, train_recall, train_thresholds = precision_recall_curve(train_y, train_probas)
+
+    return {
+        'train_pr_auc': auc(train_recall, train_precision),
+        'accuracy': accuracy_score(test_y, preds),
+        'ROC AUC': roc_auc_score(test_y, probas),
+        'PR AUC': auc(recall, precision)
+    }
+
+
 def main():
     # target = 'annotation_IBIS_PPI_INTERCHAIN'
     target = 'PPI_interface_true'
-    train = pd.read_csv('annotated_processed_training_with_alphafold_v2.csv')
+    train = pd.read_csv('annotated_processed_training_with_alphafold.csv')
     test = pd.read_csv('annotated_processed_validation_with_alphafold.csv')
     geometricus_train_path = '../datasets/PPI/geometricus_PPI_training_dataset.csv'
     geometricus_test_path = '../datasets/PPI/geometricus_PPI_validation_dataset.csv'
-    train, test = add_geometricus_features(train, test, geometricus_train_path, geometricus_test_path)
+    # train, test = add_geometricus_features(train, test, geometricus_train_path, geometricus_test_path)
     train = drop_missing_alphafold_rows(train)
     test = drop_missing_alphafold_rows(test)
 
     # train = drop_inconsistent_rows(train)
     # test = drop_inconsistent_rows(test)
     prediction_columns =[]
-    for use_geometricus in [True, False]:
+    for use_geometricus in [False]:
         for use_alphafold in [True, False]:
             for use_ff in [True, False]:
                 if use_ff == use_geometricus == use_alphafold == False:
                     continue
                 print(f'---USE ALPHAFOLD: {use_alphafold}, USE GEOMET: {use_geometricus}, USE FF: {use_ff}---')
-                predicted_probs = fit_and_evaluate(train, test, target=target, use_alphafold=use_alphafold, use_ff=use_ff, use_geometricus=use_geometricus)
+                eval_dict = fit_and_evaluate(train, test, target=target, use_alphafold=use_alphafold, use_ff=use_ff, use_geometricus=use_geometricus, use_generic=True)
+                predicted_probs = eval_dict['probas']
                 pred_colname = 'af_' + str(use_alphafold) + '_pred'
                 prediction_columns.append(pred_colname)
                 test[pred_colname] = predicted_probs
