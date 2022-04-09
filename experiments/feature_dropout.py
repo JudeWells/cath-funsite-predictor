41d03574d2a343bbacce78f3fb3b3b1c9c6e2da5
Jude <judewells@hotmail.com>
2022-04-09 14:06:57 +0100

    changes to test_alphafold_classifer

--- experiments/feature_dropout.py
+++ experiments/feature_dropout.py
@@ -4,13 +4,14 @@ import pandas as pd
 
 if __name__ == '__main__':
     target = 'PPI_interface_true'
-    train = pd.read_csv('annotated_processed_training_with_alphafold.csv')
+    train = pd.read_csv('annotated_processed_training_with_alphafold_v2.csv')
     test = pd.read_csv('annotated_processed_validation_with_alphafold.csv')
     geometricus_train_path = '../datasets/PPI/geometricus_PPI_training_dataset.csv'
     geometricus_test_path = '../datasets/PPI/geometricus_PPI_validation_dataset.csv'
-    # train, test = add_geometricus_features(train, test, geometricus_train_path, geometricus_test_path)
+    train, test = add_geometricus_features(train, test, geometricus_train_path, geometricus_test_path)
     train = drop_missing_alphafold_rows(train)
     test = drop_missing_alphafold_rows(test)
+    print(train.shape, test.shape)
     results = pd.DataFrame(index=combined_features, columns=['train_pr_auc','accuracy','ROC AUC', 'PR AUC',])
     for drop_feature in combined_features:
         if 'residue_aa' in drop_feature:
@@ -18,5 +19,6 @@ if __name__ == '__main__':
         eval_dict = fit_and_evaluate(train, test, target=target, use_alphafold=True, use_ff=True,
                                        use_geometricus=False, drop_features=[drop_feature])
         del eval_dict['probas']
-        results.loc[drop_feature, eval_dict.keys()] = eval_dict.values()
+        results.loc[drop_feature, list(eval_dict.keys())] = list(eval_dict.values())
+        results.to_csv('dropped_feature_analysis.csv')
 
